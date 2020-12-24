bf6e77c321c8ba74b515ec5202f76eac445c25ea
Your Name <you@example.com>
2020-12-24 09:25:44 +0100

    Initial Commit

---
+++ src/main/java/com/example/demo/AwsCicdTutorialApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.reactivestreams.Publisher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+import java.util.concurrent.Flow;
+
+@SpringBootApplication
+@RestController
+public class AwsCicdTutorialApplication {
+
+  private final String id = UUID.randomUUID().toString();
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwsCicdTutorialApplication.class, args);
+  }
+
+  @GetMapping("/hello")
+  public Publisher<String> hello() {
+    return Mono.just("hello - " + id);
+  }
+}
